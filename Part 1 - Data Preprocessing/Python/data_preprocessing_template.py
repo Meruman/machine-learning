7bfca59cc58781d5e9f1762d5c40a03d97bc0dc5
Meruman <36783697+Meruman@users.noreply.github.com>
2018-08-21 23:01:31 -0500

    Encoding categorical data

--- Part 1 - Data Preprocessing/Python/data_preprocessing_template.py
+++ Part 1 - Data Preprocessing/Python/data_preprocessing_template.py
@@ -12,7 +12,8 @@ dataset = pd.read_csv('Data.csv')
 X = dataset.iloc[:, :-1].values
 y = dataset.iloc[:, 3].values
 
-np.set_printoptions(threshold=np.nan)
+#np.set_printoptions(threshold=np.nan)
+
 #Taking care of missing data
 from sklearn.preprocessing import Imputer
 imputer = Imputer(missing_values = 'NaN', strategy = 'most_frequent', axis = 0) #Can be median or most_frequent instead of mean
@@ -20,6 +21,15 @@ imputer = imputer.fit(X[:, 1:3])
 X[:, 1:3] = imputer.transform(X[:, 1:3])
 
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 0)
+
+#Encoding categorical data
+from sklearn.preprocessing import LabelEncoder, OneHotEncoder #OneHotEncoder helps creating dummy variables
+labelencoder_x = LabelEncoder()
+X[:,0] = labelencoder_x.fit_transform(X[:,0]) #Transform Categories into numbers
+onehotencoder = OneHotEncoder(categorical_features = [0])
+X = onehotencoder.fit_transform(X).toarray() #Transforma el valor de las categorias, en columnas diferentes formando una especie de binarios para diferenciarlos
+labelencoder_y = LabelEncoder()
+y = labelencoder_y.fit_transform(y)
 
 # Feature Scaling
 """from sklearn.preprocessing import StandardScaler
--- Part 1 - Data Preprocessing/R/data_preprocessing_template.R
+++ Part 1 - Data Preprocessing/R/data_preprocessing_template.R
@@ -6,10 +6,23 @@ dataset = read.csv('Data.csv')
 #Taking care of missing data
 dataset$Age = ifelse(is.na(dataset$Age), 
                      ave(dataset$Age, FUN = function (X) mean(X, na.rm = TRUE)),
-                     dataset$Age)
+                     dataset$Age)   #is.na is a function that tells you if the value of the function is missing or not
 dataset$Salary = ifelse(is.na(dataset$Salary), 
                      ave(dataset$Salary, FUN = function (X) mean(X, na.rm = TRUE)),
                      dataset$Salary)
+
+#Encoding categorical data
+dataset$Country = factor(dataset$Country,
+                         levels = c('France', 'Spain', 'Germany'),
+                         labels = c(1,2,3)) #Factor hace todo el trabajo de convertir a factores las categorias
+                        #En R es mas sencillo que python porque la funcion factor convierte las categorias en numeros pero
+                        #R lo ve como factores y no niveles, los factores se lo especificamos nosotros
+
+dataset$Purchased = factor(dataset$Purchased,
+                         levels = c('No', 'Yes'),
+                         labels = c(0,1))
+
+
 
 # Splitting the dataset into the Training set and Test set
 # install.packages('caTools')
